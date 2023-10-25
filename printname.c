f0c12074047dc664cdc527afcde0fedab099505e
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-10-25 09:41:16 +0100

    This commit will name exec

---
+++ printname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * This program will get name and print it
+ */
+int main(void)
+{
+	char *name[5 + 1];
+
+	printf("please enter your name\n");
+	scanf("%s\n", &name);
+	printf("This is the name you entered\n");
+	printf("%s\n", name);
+
+	return(0);
+}
